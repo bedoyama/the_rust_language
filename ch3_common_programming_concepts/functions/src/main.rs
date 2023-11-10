063f0c019b995da1e096e5995c39b3d47483f18b
mauricio <bedoyama@gmail.com>
2023-11-10 12:41:33 -0600

    stmtsexprs

--- ch3_common_programming_concepts/functions/src/main.rs
+++ ch3_common_programming_concepts/functions/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(34);
+    print_labeled_measurement(65, 'd');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("The value of x is {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
