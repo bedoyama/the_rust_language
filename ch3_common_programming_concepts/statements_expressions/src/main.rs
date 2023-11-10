f9bd977004a94e01003a5f105d2a1502c921c04a
mauricio <bedoyama@gmail.com>
2023-11-10 13:58:34 -0600

    stmtsexprs2

--- ch3_common_programming_concepts/statements_expressions/src/main.rs
+++ ch3_common_programming_concepts/statements_expressions/src/main.rs
@@ -5,4 +5,20 @@ fn main() {
     };
 
     println!("The value of y is {y}");
+
+    let five = five();
+
+    println!("The value of five() is {five}");
+
+    let plus1 = plus_one(five);
+
+    println!("The value of plus_one() is {plus1}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
